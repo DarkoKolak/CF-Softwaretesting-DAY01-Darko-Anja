5911439adfe071951a01a3a87fd62c93d3ac091b
Anja Kuhar <anja.kuhar+x@gmail.com>
2019-07-25 10:05:17 +0200

    Jasmine testing

---
+++ calculate.js
@@ -0,0 +1,3 @@
+function cal (a, b) {
+	return a+b;
+}
---
+++ specs.js
@@ -0,0 +1,23 @@
+describe("Hello", function(){
+
+	it("should Return Hello", function(){
+
+		expect(hello()).toEqual("Hello");
+	});
+});
+
+describe("Sum", function(){
+
+	it("should return the sum", function(){
+
+		expect(cal(3,5)).toEqual(8);
+	});
+});
+
+describe("Divison", function(){
+
+	it("should return the divison", function(){
+
+		expect(divide(10,5)).toEqual(2);
+	});
+});
